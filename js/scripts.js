f5b532d32f04ac5d4f0e0ae550761dff66fc5a2e
Vitaliy Ponomaryov <vital77766688@yandex.ru>
2020-07-13 10:30:45 +0600

    Fixed header

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,15 @@ burger.onclick = () => {
 	nav.classList.toggle('active')
 	body.classList.toggle('lock')
 }
+
+const header = document.querySelector('header')
+const main = document.querySelector('main')
+window.onscroll = () => {
+	if (window.scrollY > 0) {
+		header.style.padding = '10px 0 10px'
+		main.style.margin = '64px 0 0'
+	} else {
+		header.style.padding = null
+		main.style.margin = null
+	}
+}
